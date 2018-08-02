3c5bf4a6c39ee258b6d19416ae036ea5da787e43
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-08-02 11:51:51 +0300

    implemented using arrays

---
+++ src/main/java/fxpro/hiring_test/pits_and_hills/LandscapeWaterCalculator.java
@@ -0,0 +1,114 @@
+package fxpro.hiring_test.pits_and_hills;
+
+public class LandscapeWaterCalculator {
+
+  static final int MAX_LANDSCAPE_LENGTH = 32_000;
+  static final int MAX_LANDSCAPE_HEIGHT = 32_000;
+
+  /**
+   * Takes a landscape as parameter and calculate how many water could be
+   * collected inside pits only. Max number of positions of the landscape is
+   * 32000. Height must be between 0 and 32000.
+   * 
+   * @param landscape
+   *          presented as an array of heights
+   * @return amount of water which could be collected
+   */
+  public long calculateWaterAmount(final int[] landscape) {
+
+    if (landscape.length > MAX_LANDSCAPE_LENGTH)
+      throw new IllegalArgumentException("Wrong landscape length");
+
+    if (landscape.length < 3)
+      return 0;
+
+    final LeftRightPair[] heights = new LeftRightPair[MAX_LANDSCAPE_HEIGHT + 1];
+
+    final int peakColumn;
+    {
+      int _peak = Integer.MIN_VALUE;
+      int _index = -1;
+      for (int i = 0; i < landscape.length; i++) {
+
+	if (landscape[i] < 0 || landscape[i] > MAX_LANDSCAPE_HEIGHT)
+	  throw new IllegalArgumentException(String.format("Wrong landscape height value '%1$d'", landscape[i]));
+
+	LeftRightPair pair = heights[landscape[i]];
+	if (pair == null)
+	  heights[landscape[i]] = pair = new LeftRightPair();
+	pair.acceptValue(i);
+
+	if (_peak < landscape[i]) {
+	  _peak = landscape[i];
+	  _index = i;
+	}
+      }
+      peakColumn = _index;
+    }
+
+    long result = 0;
+    final boolean[] taken = new boolean[landscape.length];
+    for (int row = heights.length - 1; row >= 0; row--) {
+
+      final LeftRightPair pair = heights[row];
+      if (pair == null)
+	continue;
+
+      int from = pair.left;
+      int to = pair.right;
+
+      if (from == to)
+	if (from > peakColumn)
+	  from = peakColumn;
+	else
+	  to = peakColumn;
+
+      for (int column = from; column <= to; column++)
+	if (landscape[column] < row && !taken[column]) {
+	  result += row - landscape[column];
+	  taken[column] = true;
+	}
+    }
+    return result;
+  }
+
+  private static class LeftRightPair {
+
+    int left = Integer.MAX_VALUE;
+    int right = Integer.MIN_VALUE;
+
+    LeftRightPair acceptValue(final int value) {
+      if (left > value)
+	left = value;
+      if (right < value)
+	right = value;
+      return this;
+    }
+  }
+
+  void printLandscape(final int[] land) {
+    if (land.length > MAX_LANDSCAPE_LENGTH)
+      throw new IllegalArgumentException("Wrong positions count");
+
+    int peak = 0;
+    for (final int colHeight : land) {
+      if (colHeight < 0 || colHeight > MAX_LANDSCAPE_HEIGHT)
+	throw new IllegalArgumentException(String.format("Wrong landscape height value '%1$d'", colHeight));
+      if (peak < colHeight)
+	peak = colHeight;
+    }
+
+    for (int row = peak; row >= 0; row--) {
+      for (final int height : land) {
+	if (height == row)
+	  System.out.print("O");
+	else if (height > row)
+	  System.out.print("|");
+	else
+	  System.out.print(" ");
+	System.out.print(" ");
+      }
+      System.out.println();
+    }
+  }
+}
---
+++ src/test/java/fxpro/hiring_test/pits_and_hills/LandscapeWaterCalculatorTest.java
@@ -0,0 +1,379 @@
+package fxpro.hiring_test.pits_and_hills;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LandscapeWaterCalculatorTest {
+
+  private LandscapeWaterCalculator calculator;
+
+  @Before
+  public void createCalculator() {
+    calculator = new LandscapeWaterCalculator();
+  }
+
+  @Test
+  public void givenToolLargeLandscapeArray_whenCalculate_thenThrowIllegalArgumentException() {
+
+    // given
+    final int[] landscape = new int[32_000 + 1];
+
+    // when
+    final Throwable thrown = catchThrowable(() -> calculator.calculateWaterAmount(landscape));
+
+    // then
+    assertThat(thrown)
+	.isInstanceOf(IllegalArgumentException.class)
+	.hasStackTraceContaining("Wrong landscape length")
+	.hasNoCause();
+  }
+
+  @Test
+  public void givenNegativeLandscapeValue_whenCalculate_thenThrowIllegalArgumentException() {
+
+    // given
+    final int[] landscape = new int[] { 1, 2, 3, 0, -100, 5 };
+
+    // when
+    final Throwable thrown = catchThrowable(() -> calculator.calculateWaterAmount(landscape));
+
+    // then
+    assertThat(thrown)
+	.isInstanceOf(IllegalArgumentException.class)
+	.hasStackTraceContaining("Wrong landscape height value")
+	.hasNoCause();
+  }
+
+  @Test
+  public void givenTooLargeLandscapeValue_whenCalculate_thenThrowIllegalArgumentException() {
+    // given
+    final int[] landscape = new int[] { 1, 320_001, 3, 0, 100, 5 };
+
+    // when
+    final Throwable thrown = catchThrowable(() -> calculator.calculateWaterAmount(landscape));
+
+    // then
+    assertThat(thrown)
+	.isInstanceOf(IllegalArgumentException.class)
+	.hasStackTraceContaining("Wrong landscape height value")
+	.hasNoCause();
+
+  }
+
+  /**
+   * The result should be '7'
+   *
+   * <pre>
+      7 |                 O
+      6 |           O O O |
+      5 |   O ~ ~ O | | | | ~ ~ O
+      4 | O | ~ O | | | | | ~ ~ |
+      3 | | | O | | | | | | O O |
+      2 | | | | | | | | | | | | |
+      1 | | | | | | | | | | | | |
+      0 | | | | | | | | | | | | |
+        +------------------------
+          0 1 2 3 4 5 6 7 8 9 0 1
+                              1
+   * </pre>
+   */
+  @Test
+  public void givenLandscapeVariant1_whenCalculate_thenReturnCorrectValue() {
+
+    // given
+    final int[] landscape = { 4, 5, 3, 4, 5, 6, 6, 6, 7, 3, 3, 5 };
+
+    // when
+    final long amount = calculator.calculateWaterAmount(landscape);
+
+    // then
+    assertThat(amount)
+	.isEqualTo(7);
+  }
+
+  /**
+   * The result should be '9'
+   *
+   * <pre>
+    O ~ ~ ~ O
+    | ~ ~ O | O
+    | ~ O | | | ~ O
+    | O | | | | ~ |
+    | | | | | | ~ | O
+    | | | | | | O | |
+   *
+   * </pre>
+   */
+  @Test
+  public void givenLandscapeVariantFromTheTask_whenCalculate_thenReturnCorrectValue() {
+
+    // given
+    final int[] landscape = { 5, 2, 3, 4, 5, 4, 0, 3, 1 };
+
+    // when
+    final long amount = calculator.calculateWaterAmount(landscape);
+
+    // then
+    assertThat(amount)
+	.isEqualTo(9);
+  }
+
+  /**
+   * The result should be '12'
+   *
+   * <pre>
+       7 |                 O
+       6 | O ~ ~ ~ ~ O O O |
+       5 | | ~ ~ ~ ~ | | | |
+       4 | | ~ ~ O ~ | | | |
+       3 | | O O | ~ | | | | O O
+       2 | | | | | O | | | | | |
+       1 | | | | | | | | | | | |
+       0 | | | | | | | | | | | |
+         +----------------------
+           0 1 2 3 4 5 6 7 8 9 0
+                               1
+   * </pre>
+   */
+  @Test
+  public void givenLandscapeVariant2_whenCalculate_thenReturnCorrectValue() {
+
+    // given
+    final int[] landscape = { 6, 3, 3, 4, 2, 6, 6, 6, 7, 3, 3 };
+
+    // when
+    final long amount = calculator.calculateWaterAmount(landscape);
+
+    // then
+    assertThat(amount)
+	.isEqualTo(12);
+  }
+
+  /**
+   * The result should be '22'
+   *
+   * <pre>
+      8 |           O
+      7 |           | ~ ~ ~ ~ O
+      6 | O ~ ~ ~ ~ | ~ ~ ~ ~ |
+      5 | | ~ ~ ~ ~ | O O O ~ |
+      4 | | ~ ~ ~ ~ | | | | ~ |
+      3 | | O O O O | | | | O |
+      2 | | | | | | | | | | | |
+      1 | | | | | | | | | | | |
+      0 | | | | | | | | | | | |
+        +----------------------
+          0 1 2 3 4 5 6 7 8 9 0
+                              1
+   * </pre>
+   */
+  @Test
+  public void givenLandscapeVariant3_whenCalculate_thenReturnCorrectValue() {
+
+    // given
+    final int[] landscape = { 6, 3, 3, 3, 3, 8, 5, 5, 5, 3, 7 };
+
+    // when
+    final long amount = calculator.calculateWaterAmount(landscape);
+
+    // then
+    assertThat(amount)
+	.isEqualTo(22);
+  }
+
+  /**
+   * The result should be '0'
+   *
+   * <pre>
+      7 |           O
+      6 |           |
+      5 |           | O O O
+      4 |           | | | |
+      3 |   O O O O | | | | O
+      2 |   | | | | | | | | |
+      1 |   | | | | | | | | |
+      0 | O | | | | | | | | | O
+        +----------------------
+          0 1 2 3 4 5 6 7 8 9 0
+                              1
+   * </pre>
+   */
+  @Test
+  public void givenLandscapeVariant5_whenCalculate_thenReturnCorrectValue() {
+
+    // given
+    final int[] landscape = { 0, 3, 3, 3, 3, 7, 5, 5, 5, 3, 0 };
+
+    // when
+    final long amount = calculator.calculateWaterAmount(landscape);
+
+    // then
+    assertThat(amount)
+	.isZero();
+  }
+
+  /**
+   * The result should be '8'
+   *
+   * <pre>
+      7 |         O
+      6 |         | ~ O
+      5 |       O | ~ |
+      4 | O ~ ~ | | ~ |
+      3 | | ~ O | | ~ |
+      2 | | O | | | ~ |
+      1 | | | | | | O |
+      0 | | | | | | | |
+        +--------------
+          0 1 2 3 4 5 6
+   * </pre>
+   */
+  @Test
+  public void givenLandscapeVariant6_whenCalculate_thenReturnCorrectValue() {
+
+    // given
+    final int[] landscape = { 4, 2, 3, 5, 7, 1, 6 };
+
+    // when
+    final long amount = calculator.calculateWaterAmount(landscape);
+
+    // then
+    assertThat(amount)
+	.isEqualTo(8);
+  }
+
+  /**
+   * The result should be '21'
+   *
+   * <pre>
+      7 |               O
+      6 |               | ~ O    1
+      5 |       O ~ ~ ~ | ~ |    4
+      4 | O ~ ~ | ~ ~ ~ | ~ |    6
+      3 | | ~ O | ~ ~ ~ | ~ |    5
+      2 | | O | | ~ O ~ | ~ |    3
+      1 | | | | | ~ | ~ | O |    2
+      0 | | | | | O | O | | |    0
+        +--------------------
+          0 1 2 3 4 5 6 7 8 9
+   *
+   * </pre>
+   */
+  @Test
+  public void givenLandscapeVariant7_whenCalculate_thenReturnCorrectValue() {
+
+    // given 0 1 2 3 4 5 6 7 8 9
+    final int[] landscape = { 4, 2, 3, 5, 0, 2, 0, 7, 1, 6 };
+
+    // when
+    final long amount = calculator.calculateWaterAmount(landscape);
+
+    // then
+    assertThat(amount)
+	.isEqualTo(21);
+  }
+
+  /**
+   * The result should be '2'
+   *
+   * <pre>
+      4 | O
+      3 | | ~ O
+      2 | | ~ |
+      1 | | O |
+      0 | | | |
+        +------
+          0 1 2
+   * </pre>
+   */
+  @Test
+  public void givenLandscapeVariant4_whenCalculate_thenReturnCorrectValue() {
+
+    // given
+    final int[] landscape = { 4, 1, 3 };
+
+    // when
+    final long amount = calculator.calculateWaterAmount(landscape);
+
+    // then
+    assertThat(amount)
+	.isEqualTo(2);
+  }
+
+  @Test
+  public void givenEmptyLandscape_whenCalculate_thenReturnZeroValue() {
+
+    // given
+    final int[] landscape = {};
+
+    // when
+    final long amount = calculator.calculateWaterAmount(landscape);
+
+    // then
+    assertThat(amount)
+	.isZero();
+  }
+
+  @Test
+  public void givenSignleValueLandscape_whenCalculate_thenReturnZeroValue() {
+
+    // given
+    final int[] landscape = { 1 };
+
+    // then
+    final long amount = calculator.calculateWaterAmount(landscape);
+
+    // when
+    assertThat(amount)
+	.isZero();
+  }
+
+  @Test
+  public void givenDoubleValueLandscape_whenCalculate_thenReturnZeroValue() {
+
+    // given
+    final int[] landscape = { 2, 3 };
+
+    // then
+    final long amount = calculator.calculateWaterAmount(landscape);
+
+    // when
+    assertThat(amount)
+	.isZero();
+  }
+
+  @Test
+  public void givenNullLandscape_whenCalculate_thenThrowNullPointerException() {
+
+    // given
+    final int[] landscape = null;
+
+    // when
+    final Throwable thrown = catchThrowable(() -> calculator.calculateWaterAmount(landscape));
+
+    // then
+    assertThat(thrown)
+	.isInstanceOf(NullPointerException.class)
+	.hasNoCause();
+
+  }
+
+  @Test
+  public void givenBeerMugLandscape_whenCalculate_thenReturnCorrectValue() {
+
+    // given
+    final int[] landscape = new int[32_000];
+    landscape[0] = 32_000;
+    landscape[32_000 - 1] = 32_000;
+
+    // when
+    final long amount = calculator.calculateWaterAmount(landscape);
+
+    // then
+    assertThat(amount)
+	.isEqualTo(32_000L * (32_000 - 2));
+
+  }
+}
